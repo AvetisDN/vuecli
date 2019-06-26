ac91230c2a1d97f787af1d007dc3c820b77002d0
AvetisDN <avetisdn@gmail.com>
2019-06-26 17:59:47 +0300

    1

--- src/main.js
+++ src/main.js
@@ -2,9 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+const config = {
+  apiKey: "AIzaSyDqCsQ0l_BhGgNYYqoYQ4E5vwVPjkveKZc",
+  authDomain: "to-do-list-55dd1.firebaseapp.com",
+  databaseURL: "https://to-do-list-55dd1.firebaseio.com",
+  projectId: "to-do-list-55dd1",
+  storageBucket: "",
+  messagingSenderId: "438378635283",
+  appId: "1:438378635283:web:199ce54a3a3f0844"
+};
+
+firebase.initializeApp(config)
+
+export const db = firebase.firestore()
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,94 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="@/assets/logo.png" id="vue-logo">
+    <div class="title">What do I need to do today?</div>
+    <input v-model="myTodo" /><button @click="addToDo">Add</button>
+    <div v-if="errors !== ''" id="errors">{{ errors }}</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import { db } from '@/main'
+  export default {
+    name: 'home',
+    data: function () {
+      return {
+        myTodo: '',
+        errors: ''
+      }
+    },
+    methods: {
+      addToDo: function () {
+        this.errors = ''
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+        if (this.myTodo !== '') {
+          db.collection('items').add({
+            title: this.myTodo,
+            created_at: Date.now()
+          }).then((response) => {
+            if (response) {
+              this.myTodo = ''
+            }
+          }).catch((error) => {
+            this.errors = error
+          })
+        } else {
+          this.errors = 'Please enter some text'
+        }
+      }
+    }
   }
-}
 </script>
+
+<style>
+  * {
+    box-sizing:border-box;
+  }
+
+  body, html, #app {
+    background: #eaf1ee;
+  }
+
+  .home {
+    width:300px;
+    margin:auto;
+  }
+
+  #vue-logo {
+    width:100px;
+  }
+
+  input, button {
+    border:0;
+    width:100%;
+    margin:0 0 10px;
+    padding:7px;
+  }
+
+  input {
+    font-size:12px;
+  }
+
+  button {
+    background:#43b823;
+    border:0;
+    text-transform:uppercase;
+    color:#fff;
+    font-weight:700;
+    cursor:pointer;
+  }
+
+  .title {
+    font-size:14px;
+    font-weight:700;
+    padding:0 0 10px 0;
+    margin:0 0 10px 0;
+    border-bottom:1px solid #666;
+  }
+
+  #errors {
+    background:#a52222;
+    color:#fff;
+    padding:5px;
+  }
+</style>
